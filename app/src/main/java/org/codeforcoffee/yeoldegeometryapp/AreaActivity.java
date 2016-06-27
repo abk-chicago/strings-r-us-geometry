4b48ddd1d79e23accf2d25ce1f301ad9b2cd29d3
Andrea Kim <andreaforanimals@gmail.com>
2016-06-27 12:35:52 -0500

    going well

--- app/src/main/java/org/codeforcoffee/yeoldegeometryapp/AreaActivity.java
+++ app/src/main/java/org/codeforcoffee/yeoldegeometryapp/AreaActivity.java
@@ -9,5 +9,9 @@ public class AreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area);
+
+
+
+
     }
 }
